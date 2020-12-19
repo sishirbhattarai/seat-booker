6b289e7c2ff9e157dfa1317bec19ac7b559454cf
Jared Lim <jaredlim1@gmail.com>
2020-12-19 13:24:06 -0700

    initial route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,12 @@ const { Production, Showing, Seat, Ticket } = require('../models');
 router.get('/', async (req, res) => {
   try {
     const productionData = await Production.findAll({
-      include: [{Showing}, {Seat}, {Ticket}]
+      include: [{
+        model: Showing,
+        include: [Seat, Ticket]
+      }]
     })
+    console.log("Production Daeta", productionData)
     res.status(200).json(productionData)
   } catch (err) {
     res.status(500).json(err)
--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ const Ticket = require('./Ticket');
 
 Production.hasMany(Showing, {
   foreignKey: 'production_id',
-  as: 'showing_production_id'
 });
 
   Showing.belongsTo(Production, {
@@ -15,34 +14,26 @@ Production.hasMany(Showing, {
 
 Showing.hasMany(Seat, {
   foreignKey: 'showing_id',
-  as: 'seat_showing_id'
 });
 
-  // Seat.belongsToMany(Showing, {
-  //   through: Ticket,
-  //   as: 'showing_seat_id',
-  //   foreignKey: 'showing_id',
-  // });
+Showing.hasMany(Ticket, {
+  foreignKey: 'showing_id',
+});
+
+
 
 // Ticket
 Ticket.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Ticket.belongsTo(Production, {
-  // foreignKey: 'production_id'
-});
-
 Ticket.belongsTo(Showing, {
-  // foreignKey: 'showing_id'
+  foreignKey: 'showing_id'
 });
 
 Ticket.belongsTo(Seat, {
   foreignKey: 'seat_id'
 });
 
-User.hasMany(Ticket, {
-  foreignKey: 'ticket_id'
-});
 
 module.exports = { User, Production, Showing, Seat, Ticket };
