334c88c5e2ee02c24c0cdf45027a027435d88ec9
lfelsted <logansfelsted@gmail.com>
2020-12-21 13:36:02 -0700

    added routers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Production, Showing, Seat, Ticket } = require('../models');
+const { Production, Showing, Seat, Ticket, User } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -15,6 +15,47 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err)
   }  
+});
+
+router.get('/production/:id', async (req, res) => {
+  try {
+    const productionData = await Production.findByPk(req.params.id, {
+      include: [{
+        model: Showing,
+        include: [Seat, Ticket]
+      }]
+    })
+    res.status(200).json(productionData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/profile', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ["password"]
+      },
+      include: [{
+        model: Ticket
+      }],
+    }) 
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  } 
+  res.render('login');
 });
 
 module.exports = router;
