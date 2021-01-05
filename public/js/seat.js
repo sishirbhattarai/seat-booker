33766ddaf86f3e38ae09b1dece9a0048ab823f57
Jared Lim <jaredlim1@gmail.com>
2021-01-05 14:20:26 -0700

    dynamic seat selected and desection for stubs

--- public/js/seat.js
+++ public/js/seat.js
@@ -1,3 +1,5 @@
+// const { eq } = require("sequelize/types/lib/operators");
+
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
@@ -6,24 +8,27 @@ const total = document.getElementById('total');
 const selectedSeats = document.getElementsByClassName('selected-seats');
 const seat = document.querySelector('.container .seat');
 
-
-// const movieSelect = document.getElementById('movie');
-
-// let ticketPrice = +movieSelect.value;
-
 //Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
-  // total.innerText = selectedSeatsCount * ticketPrice;
+  checkoutBtn();
 }
 
-//Movie Select Event
-// movieSelect.addEventListener('change', e => {
-//   ticketPrice = +e.target.value;
-//   updateSelectedCount();
-// });
+function checkoutBtn() {
+  const checkout = $('.initiate-checkout')
+  const btn = `<button class="btn btn-primary btn-block">Proceed to Checkout</button>`
+  checkout.text("")
+  const seatCount = $('#count').text()
+  console.log("seatCount", seatCount)
+
+  if (seatCount === '0') {
+    checkout.text("") 
+  } else {
+    checkout.append(btn)
+  }
+}
 
 //Seat click event
 container.addEventListener('click', e => {
@@ -38,55 +43,83 @@ container.addEventListener('click', e => {
 });
 
 
-$(".seat").on('click', e => {
+
+
+
+$(".seat").on('click', function (e) {
   e.preventDefault();
 
-  console.log("test test")
+  const id = $(this).attr('id');
+  const row = $(this).attr('data-row');
+  const seat = $(this).attr('data-seat');
+  const price = $(this).attr('data-price');
+  const stubs = $('.selected-seats')
+
+  x = $(this).attr('class')
+
+  if (x === "seat selected") {
+    $('#stub-'+id).remove();
+  } else {
+
+
+  const info = 
+  `<div id='stub-${id}'>
+  <div class="row">
+  <article class="card ticket fl-left seat-selection">
+     <section class="date-showing">
+      <time>
+        <span class="seat-info-bold">${row}${seat}</span>
+      </time>
+    </section>
+    <section class="card-cont">
+      <small>SeatBooker Theaters Seating Info</small>
+    <div class="even-seat">
+        <i class="fa fa-star" style="padding-right: 15px"></i>
+        <span class="seat-info">Seat: ${row}${seat}</span>
+    </div>
+      <div class="even-info">
+        <i class="fa fa-dollar"></i>
+        <span class="price-info">${price}</span>
+      </div>
+    </section>
+  </article>
+  </div>
+  </div>`
+
+  stubs.append(info)
+  }
+
+    // $('#stub-'+id).remove();
+
+    // const arr = []
+    // const x = stubs.children()
+    // arr.push(x)
+  
+    // const y = stubs.children().attr('id')
+
+    // for (var i = 0; i < arr.length; i++) {
+    //   console.log("id", arr[i].attr('id'))
+    //   console.log("array", arr)
+    // }
+
+
+
+
 })
 
-seat.addEventListener('click', e => {
+
+$(".seat").on('click', function (e) {
   e.preventDefault();
-  e.stopPropagation();
 
-  console.log("test test") 
-  // const seat  
-  // const price 
-  
-  // const stub 
-// `<div class="row">
-//   <article class="card ticket fl-left seat-selection">
-//      <section class="date-showing">
-//       <time>
-//         <span class="seat-info-bold">A7</span>
-//       </time>
-//     </section>
-//     <section class="card-cont">
-//       <small>SeatBooker Theaters Seating Info</small>
-//     <div class="even-seat">
-//         <i class="fa fa-star" style="padding-right: 15px"></i>
-//         <span class="seat-info">Seat: A7</span>
-//     </div>
-//       <div class="even-info">
-//         <i class="fa fa-dollar"></i>
-//         <span class="price-info">$70.00</span>
-//       </div>
-//     </section>
-//   </article>
-//   </div>`
+  x = $(this).attr('class')
+  console.log("x", x)
+
+})
 
 
 
-});
 
-
-if (!selectedSeats) {
-  // don't generate the button
-} else {
-  // initiate-checkout div.append
-  // <button class="btn btn-primary btn-block">Proceed to Checkout</button>
-
-}
-
+ 
 
 // const purchaseTickets = async(event) => {
 //   event.preventDefault();
